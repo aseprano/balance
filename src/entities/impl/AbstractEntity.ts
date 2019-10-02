88a5eec0c8e40959a436e829d25740a3ca6bf847
Antonio Seprano <antonio.seprano@docebo.com>
2019-10-02 22:42:59 +0200

    Added common code to AbstractEntity

---
+++ src/entities/impl/AbstractEntity.ts
@@ -0,0 +1,30 @@
+import { EventStream } from "../../tech/EventStream";
+import { Event } from "../../tech/Event";
+import { Entity } from "../Entity";
+import { DomainEvent } from "../../events/DomainEvent";
+
+export abstract class AbstractEntity implements Entity {
+    private currentStreamVersion: number = 0;
+    private uncommittedEvents: DomainEvent[] = [];
+
+    protected appendUncommittedEvent(event: DomainEvent): void {
+        this.uncommittedEvents.push(event);
+        this.applyEvent(event);
+    }
+
+    public restoreFromEventStream(stream: EventStream): void {
+        stream.events.forEach(e => this.applyEvent(e));
+        this.currentStreamVersion = stream.version;
+    }
+
+    public getVersion(): number {
+        return this.currentStreamVersion;
+    }
+
+    public commitEvents(): DomainEvent[] {
+        return this.uncommittedEvents.splice(0);
+    }
+
+    protected abstract applyEvent(event: Event): void;
+    
+}
