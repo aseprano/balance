7c8cb5b2c67733a48c17a3422c03ad1d3392168b
Antonio Seprano <antonio.seprano@docebo.com>
2019-10-04 23:45:47 +0200

    Added BankAccount implementation and relative test

---
+++ src/entities/impl/BankAccountImpl.spec.ts
@@ -0,0 +1,103 @@
+import { BankAccountImpl } from "./BankAccountImpl";
+import { AccountID } from "../../values/AccountID";
+import { AccountCreatedEvent } from "../../events/AccountCreatedEvent";
+import { Money } from "../../values/Money";
+import { AccountCreditedEvent } from "../../events/AccountCreditedEvent";
+import { AccountDebitedEvent } from "../../events/AccountDebitedEvent";
+
+describe('BankAccountImpl', () => {
+
+    const testId = new AccountID('12332100012');
+
+    it('can be initalized with an account id', () => {
+        const account = new BankAccountImpl();
+        account.initialize(testId);
+
+        expect(account.getId())
+            .toEqual(testId);
+
+        expect(account.commitEvents())
+            .toEqual([
+                new AccountCreatedEvent(testId)
+            ]);
+    });
+
+    it('can be reconstructed from events', () => {
+        const account = new BankAccountImpl();
+        account.restoreFromEventStream({
+            version: 10,
+            events: [
+                new AccountCreatedEvent(testId),
+                new AccountCreditedEvent(testId, new Money(1000, 'EUR')),
+                new AccountCreditedEvent(testId, new Money(750, 'USD')),
+                new AccountDebitedEvent(testId, new Money(200, 'EUR')),
+                new AccountDebitedEvent(testId, new Money(0.8, 'USD')),
+            ]
+        });
+
+        expect(account.getId()).toEqual(testId);
+        expect(account.getBalance('EUR')).toBeCloseTo(800, 0);
+        expect(account.getBalance('USD')).toBeCloseTo(749.2, 0);
+        expect(account.getVersion()).toEqual(10);
+    });
+
+    it('has a balance of zero after initialization', () => {
+        const account = new BankAccountImpl();
+        expect(account.getBalance('EUR')).toEqual(0);
+        expect(account.getBalance('USD')).toEqual(0);
+    });
+
+    it('can be credited', () => {
+        const account = new BankAccountImpl();
+        account.restoreFromEventStream({
+            version: 10,
+            events: [
+                new AccountCreatedEvent(testId)
+            ]
+        });
+
+        account.credit(new Money(3.14, 'EUR'));
+
+        expect(account.getBalance('EUR')).toBeCloseTo(3.14, 0);
+        expect(account.getBalance('USD')).toEqual(0);
+        expect(account.commitEvents()).toEqual([
+            new AccountCreditedEvent(testId, new Money(3.14, 'EUR'))
+        ]);
+    });
+
+    it('can be debited when balance is enough', () => {
+        const account = new BankAccountImpl();
+        account.restoreFromEventStream({
+            version: 10,
+            events: [
+                new AccountCreatedEvent(testId),
+                new AccountCreditedEvent(testId, new Money(1000, 'EUR'))
+            ]
+        });
+
+        account.debit(new Money(100.12, 'EUR'));
+        expect(account.getBalance('EUR')).toBeCloseTo(899.88, 0);
+        expect(account.commitEvents()).toEqual([
+            new AccountDebitedEvent(testId, new Money(100.12, 'EUR'))
+        ]);
+    });
+
+    it('cannot be debited it balance is not enough', () => {
+        const account = new BankAccountImpl();
+        account.restoreFromEventStream({
+            version: 10,
+            events: [
+                new AccountCreatedEvent(testId),
+                new AccountCreditedEvent(testId, new Money(1000, 'EUR')),
+            ]
+        });
+
+        expect(() => {
+            account.debit(new Money(1000.01, 'EUR'));
+        }).toThrow();
+
+        expect(account.getBalance('EUR')).toBeCloseTo(1000, 0);
+        expect(account.commitEvents()).toEqual([]);
+    });
+
+});
---
+++ src/entities/impl/BankAccountImpl.ts
@@ -0,0 +1,80 @@
+import { AbstractEntity } from "./AbstractEntity";
+import { BankAccount } from "../BankAccount";
+import { AccountID } from "../../values/AccountID";
+import { Money } from "../../values/Money";
+import { Event } from "../../tech/Event";
+import { AccountCreatedEvent } from "../../events/AccountCreatedEvent";
+import { AccountDebitedEvent } from "../../events/AccountDebitedEvent";
+import { AccountCreditedEvent } from "../../events/AccountCreditedEvent";
+
+export class BankAccountImpl extends AbstractEntity implements BankAccount {
+    private accountId?: AccountID;
+    private balances: Map<string, number> = new Map();
+
+    private parseMoney(amount: number, currency: string): Money {
+        return new Money(amount, currency);
+    }
+
+    private applyAccountCreatedEvent(event: Event) {
+        this.accountId = new AccountID(event.getPayload()['id']);
+    }
+
+    private applyDebitEvent(event: Event) {
+        const debitCurrency = event.getPayload()['debit']['currency'];
+        const currentBalance = this.getBalance(debitCurrency);
+        const newBalance = currentBalance - event.getPayload()['debit']['amount'];
+        this.balances.set(debitCurrency, newBalance);
+    }
+
+    private applyCreditEvent(event: Event) {
+        const creditCurrency = event.getPayload()['credit']['currency'];
+        const currentBalance = this.getBalance(creditCurrency);
+        const newBalance = currentBalance + event.getPayload()['credit']['amount'];
+        this.balances.set(creditCurrency, newBalance);
+    }
+
+    protected applyEvent(event: Event): void {
+        switch (event.getName()) {
+            case AccountCreatedEvent.EventName:
+                this.applyAccountCreatedEvent(event);
+                break;
+
+            case AccountDebitedEvent.EventName:
+                this.applyDebitEvent(event);
+                break;
+
+            case AccountCreditedEvent.EventName:
+                this.applyCreditEvent(event);
+                break;
+        }
+    }
+
+    private hasEnoughBalance(requestedAmount: Money): boolean {
+        return this.getBalance(requestedAmount.getCurrency()) >= requestedAmount.getAmount();
+    }
+
+    public initialize(id: AccountID): void {
+        this.appendUncommittedEvent(new AccountCreatedEvent(id));
+    }
+    
+    getId(): AccountID {
+        return this.accountId!;
+    }
+    
+    debit(amount: Money): void {
+        if (!this.hasEnoughBalance(amount)) {
+            throw new Error('Insufficient funds');
+        }
+
+        this.appendUncommittedEvent(new AccountDebitedEvent(this.getId(), amount));
+    }
+
+    credit(amount: Money): void {
+        this.appendUncommittedEvent(new AccountCreditedEvent(this.getId(), amount));
+    }
+
+    getBalance(currency: string): number {
+        return this.balances.get(currency) || 0;
+    }
+
+}
